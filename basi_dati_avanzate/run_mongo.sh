ae90b77cabf0d66082c831e654482599610bddcb
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-26 23:55:28 +0200

    updated docker script

--- basi_dati_avanzate/run_mongo.sh
+++ basi_dati_avanzate/run_mongo.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if command -v jq &>/dev/null; then
-    if ! docker ps --format '{{json .}}' | jq -e -s 'any(.[]; .Names == "mongo-server")' >/dev/null; then
-        docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
-    fi
-else
-    docker stop mongo-server
+if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
     docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
 fi
