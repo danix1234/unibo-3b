24d45c6d003332ff469cb317daa9f7330a47ead5
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-27 16:23:48 +0200

    fixed script

--- basi_dati_avanzate/mongobd/run_query.sh
+++ basi_dati_avanzate/mongobd/run_query.sh
@@ -4,5 +4,4 @@ export SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 export SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 export QUERY_FILE="${SCRIPT_DIR}/queries.js"
 
-echo "${QUERY_FILE}" |
-    entr sh -c 'clear; mongosh --quiet --eval "try { $(sed "s://.*::" "$QUERY_FILE" | tr -d "\n" ) } catch(e) {print(e); quit(0);}"'
+echo "${QUERY_FILE}" | entr bash -c "clear; sed '/^[[:space:]]*$/d; /^[[:space:]]*\/\//d' $QUERY_FILE | mongosh --quiet"
