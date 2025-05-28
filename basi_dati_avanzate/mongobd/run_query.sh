a6c15ae4b9112fc7c2a5b555ae3306bfee223220
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-28 15:21:27 +0200

    updated script

--- basi_dati_avanzate/mongobd/run_query.sh
+++ basi_dati_avanzate/mongobd/run_query.sh
@@ -5,9 +5,4 @@ DB_URL="$2"
 
 [[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
 
-if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
-    docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
-    sleep 0.5
-fi &>/dev/null
-
-echo "${QUERY_FILE}" | entr bash -c "clear; sed '/^[[:space:]]*$/d; /^[[:space:]]*\/\//d' $QUERY_FILE | docker exec -i mongo-server mongosh --quiet $DB_URL"
+echo "$QUERY_FILE" | entr -c mongosh --quiet "$DB_URL" --file "$QUERY_FILE"
