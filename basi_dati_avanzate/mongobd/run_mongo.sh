5d207ea899540b5c00d6876ed05d7a187a4c2790
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-29 08:25:55 +0200

    improved script

--- basi_dati_avanzate/mongobd/run_mongo.sh
+++ basi_dati_avanzate/mongobd/run_mongo.sh
@@ -3,12 +3,35 @@
 QUERY_FILE="$1"
 DB_URL="$2"
 
-if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
-    echo "launching mongo server in a docker container..." >/dev/tty
-    docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
-fi &>/dev/null
+function launch_server() {
+    if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
+        echo "launching the mongo container..." >/dev/tty
+        docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
+    else
+        echo "the mongo container is already running!" >/dev/tty
+    fi &>/dev/null
+}
+function stop_server() {
+    if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 1 ]]; then
+        echo "stopping the mongo container..." >/dev/tty
+        docker stop mongo-server
+    else
+        echo "mongo container is already not running!" >/dev/tty
+    fi &>/dev/null
+}
 
-[[ -z "$QUERY_FILE" ]] && exit 0
+case "$QUERY_FILE" in
+"end" | "stop")
+    stop_server
+    exit 0
+    ;;
+"launch" | "run")
+    launch_server
+    exit 0
+    ;;
+esac
+
 [[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
+launch_server
 
-echo "$QUERY_FILE" | entr sh -c "clear && docker cp $QUERY_FILE mongo-server:/tmp/query.js >/dev/null && mongosh --quiet $DB_URL --file /tmp/query.js"
+echo "$QUERY_FILE" | entr sh -c "clear && docker cp $QUERY_FILE mongo-server:/tmp/query.js >/dev/null && docker exec -t mongo-server mongosh --quiet $DB_URL --file /tmp/query.js"
