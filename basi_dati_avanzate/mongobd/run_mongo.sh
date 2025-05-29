dba3f5642836ce7b48072c0d824dec6d3863e9d4
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-29 08:00:35 +0200

    improved script file

--- basi_dati_avanzate/mongobd/run_mongo.sh
+++ basi_dati_avanzate/mongobd/run_mongo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+QUERY_FILE="$1"
+DB_URL="$2"
+
 if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
+    echo "launching mongo server in a docker container..." >/dev/tty
     docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 mongo
-fi
+fi &>/dev/null
+
+[[ -z "$QUERY_FILE" ]] && exit 0
+[[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
+
+echo "$QUERY_FILE" | entr sh -c "clear && docker cp $QUERY_FILE mongo-server:/tmp/query.js >/dev/null && mongosh --quiet $DB_URL --file /tmp/query.js"
--- basi_dati_avanzate/mongobd/run_query.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-QUERY_FILE="$1"
-DB_URL="$2"
-
-[[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
-
-echo "$QUERY_FILE" | entr sh -c "clear; mongosh --quiet $DB_URL --file $QUERY_FILE"
