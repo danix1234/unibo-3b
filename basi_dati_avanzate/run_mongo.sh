7237206a116ae40bf39478e9686a711287cef310
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-29 12:18:26 +0200

    remove port in docker container creation (docker network = none)

--- basi_dati_avanzate/run_mongo.sh
+++ basi_dati_avanzate/run_mongo.sh
@@ -5,7 +5,7 @@ QUERY_FILE="$1"
 function launch_server() {
     if [[ "$(docker ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
         echo "launching the mongo container..." >/dev/tty
-        docker run --rm -d -v mongodb_data:/data/db --name mongo-server -p 27017:27017 --network none mongo
+        docker run --rm -d -v mongodb_data:/data/db --name mongo-server --network none mongo
         sleep 1
     else
         echo "the mongo container is already running!" >/dev/tty
