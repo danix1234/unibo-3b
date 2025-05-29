f0e0b0ac0d1eb3b0c140c19815fe4188a471e9f1
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-05-29 08:36:20 +0200

    added help msg

--- basi_dati_avanzate/mongobd/run_mongo.sh
+++ basi_dati_avanzate/mongobd/run_mongo.sh
@@ -20,18 +20,36 @@ function stop_server() {
     fi &>/dev/null
 }
 
-case "$QUERY_FILE" in
+case "$*" in
 "end" | "stop")
     stop_server
-    exit 0
     ;;
 "launch" | "run")
     launch_server
-    exit 0
+    ;;
+"show" | "ps")
+    docker ps -a
+    ;;
+"help" | "-h" | "--help" | "")
+    echo "
+Program to launch mongodb server, and run query file on a tracked file
+
+Usage: ./run_mongo.sh [OPTION]|[QUERY_FILE [DB_URL]]
+
+Options:
+    end,stop            stop the mongo server
+    launch,run          start the mongo server
+    show,ps             show running containers
+    help,-h,--help      print this help message
+
+Otherwise: 
+    (1) QUERY_FILE      file with mongo queries which will be run every time it is saved
+    (2) [DB_URL]        url for the server to connect to (if empty: connects to local server)
+"
+    ;;
+*)
+    [[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
+    launch_server
+    echo "$QUERY_FILE" | entr sh -c "clear && docker cp $QUERY_FILE mongo-server:/tmp/query.js >/dev/null && docker exec -t mongo-server mongosh --quiet $DB_URL --file /tmp/query.js"
     ;;
 esac
-
-[[ ! -f "$QUERY_FILE" ]] && echo "not a file: $QUERY_FILE" && exit 1
-launch_server
-
-echo "$QUERY_FILE" | entr sh -c "clear && docker cp $QUERY_FILE mongo-server:/tmp/query.js >/dev/null && docker exec -t mongo-server mongosh --quiet $DB_URL --file /tmp/query.js"
