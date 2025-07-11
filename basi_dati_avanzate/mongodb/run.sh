0336ec28a1a66f027afb79df8b77b6ec3b76e357
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-11 10:11:11 +0200

    added restart option

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -3,10 +3,14 @@
 # notes:
 # - /tmp/query.js is the file to edit to make queries (do not delete!)
 # - requires `mongo-client` image, built from dockerfile
+#
+# paramters:
+#   - kill|stop|rm  -> stop container
+#   - restart       -> restart container
 
-if [[ "$1" =~ kill|stop|rm ]]; then
+if [[ "$1" =~ kill|stop|rm|restart ]]; then
     podman kill mongo-client &>/dev/null && echo "container removed"
-    exit 0
+    [[ "$1" =~ kill|stop|rm ]] && exit 0
 fi
 
 if ! podman container inspect -f '{{.State.Running}}' mongo-client &>/dev/null; then
