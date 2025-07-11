0e074fb7d4523f23ea3f570c3757579c36f922ca
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-11 16:21:31 +0200

    working oracle querying client

--- basi_dati_avanzate/oracledb/run.sh
+++ basi_dati_avanzate/oracledb/run.sh
@@ -4,5 +4,6 @@
 #   - /tmp/query.sql is the file to edit to make queries
 #   - requires `oracle-client` image, built from dockerfile
 
+DB_URL="$1"
 touch /tmp/query.sql
-podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.sql:/query.sql:ro localhost/oracle-client sh -c 'ls /query.sql | entr sh -c ""'
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.sql:/query.sql:ro localhost/oracle-client sh -c 'ls /query.sql | entr -c sh -c "echo \"\$(cat /query.sql)\" | sqlplus -s '"$DB_URL"'"'
