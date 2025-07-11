1dfb40fccacf7c553de22fbac6abe3ed0e71f98f
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-11 10:05:37 +0200

    added logic to handle query on file save

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -13,3 +13,5 @@ if ! podman container inspect -f '{{.State.Running}}' mongo-client &>/dev/null; 
     touch /tmp/query.js
     podman run --rm -d --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro --name mongo-client localhost/mongo-client >/dev/null && echo "container launched"
 fi
+
+podman exec -it mongo-client sh -c 'ls /query.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /query.js)\""'
