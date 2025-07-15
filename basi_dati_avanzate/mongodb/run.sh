e8c9d3525f835d996b3d044cfda5576e1034b81a
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-15 17:43:18 +0200

    mongodb now does proper cleanup on exit

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 # notes:
-#   - /tmp/query.js is the file to edit to make queries 
-#   - requires `mongo-client` image from ghcr.io
-#
-# paramters:
-#   - kill|stop|rm  -> stop container
-#   - restart       -> restart container
+#   - /tmp/query.js is the file to edit to make queries
+#   - requires `mongo-client` image from ghcr.io server
 
-if [[ "$1" =~ kill|stop|rm|restart ]]; then
-    podman kill mongo-client &>/dev/null && echo "container removed"
-    [[ "$1" =~ kill|stop|rm ]] && exit 0
-fi
-
-if ! podman container inspect -f '{{.State.Running}}' mongo-client &>/dev/null; then
-    touch /tmp/query.js
-    podman run --rm -d --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro --name mongo-client ghcr.io/danix1234/mongo-client >/dev/null && echo "container launched"
-fi
-
-podman exec -it mongo-client sh -c 'ls /query.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /query.js)\""'
+touch /tmp/query.js
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro --name mongo-client ghcr.io/danix1234/mongo-client sh -c 'mongod --logpath /dev/null & ls /query.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /query.js)\""'
