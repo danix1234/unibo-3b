cad7d0b9b1011c74937a90801d7d44c1da129cd5
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-15 15:28:01 +0200

    save mongodb databse in named mount

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -15,7 +15,7 @@ fi
 
 if ! podman container inspect -f '{{.State.Running}}' mongo-client &>/dev/null; then
     touch /tmp/query.js
-    podman run --rm -d --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro --name mongo-client ghcr.io/danix1234/mongo-client >/dev/null && echo "container launched"
+    podman run --rm -d --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro -v mongo-database:/data/db --name mongo-client ghcr.io/danix1234/mongo-client >/dev/null && echo "container launched"
 fi
 
 podman exec -it mongo-client sh -c 'ls /query.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /query.js)\""'
