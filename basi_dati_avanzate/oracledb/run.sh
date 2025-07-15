6e3e6d233c5e2eb4939642943186ca34720ca5ae
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-15 15:17:59 +0200

    fixed images to use ghcr.io instead of localhost

--- basi_dati_avanzate/oracledb/run.sh
+++ basi_dati_avanzate/oracledb/run.sh
@@ -2,8 +2,8 @@
 
 # notes:
 #   - /tmp/query.sql is the file to edit to make queries
-#   - requires `oracle-client` image, built from dockerfile
+#   - requires `oracle-client` image from ghcr.io server
 
 DB_URL="$1"
 touch /tmp/query.sql
-podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.sql:/query.sql:ro localhost/oracle-client sh -c 'ls /query.sql | entr -c sh -c "echo \"\$(cat /query.sql)\" | sqlplus -s '"$DB_URL"'"'
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.sql:/query.sql:ro ghcr.io/danix1234/oracle-client sh -c 'ls /query.sql | entr -c sh -c "echo \"\$(cat /query.sql)\" | sqlplus -s '"$DB_URL"'"'
