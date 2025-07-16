433fc0a6c257ee0f7c0fa5c22c1bc30da8ec5e44
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-16 11:29:53 +0200

    improved run scripts

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# notes:
-#   - /tmp/query.js is the file to edit to make queries
-#   - requires `mongo-client` image from ghcr.io server
-
-touch /tmp/query.js
-podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.js:/query.js:ro --name mongo-client ghcr.io/danix1234/mongo-client sh -c 'mongod --logpath /dev/null & ls /query.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /query.js)\""'
+mkdir -p /tmp/queries
+touch /tmp/queries/mongo.js
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/queries:/host:ro --name mongo-client ghcr.io/danix1234/mongo-client sh -c 'mongod --logpath /dev/null & ls /host/mongo.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /host/mongo.js)\""'
--- basi_dati_avanzate/oracledb/run.sh
+++ basi_dati_avanzate/oracledb/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# notes:
-#   - /tmp/query.sql is the file to edit to make queries
-#   - requires `oracle-client` image from ghcr.io server
-
 DB_URL="$1"
-touch /tmp/query.sql
-podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/query.sql:/query.sql:ro ghcr.io/danix1234/oracle-client sh -c 'ls /query.sql | entr -c sh -c "echo \"\$(cat /query.sql)\" | sqlplus -s '"$DB_URL"'"'
+[[ -z "$DB_URL" ]] && echo 'missing url for oracle connection' && return 1
+mkdir -p /tmp/queries
+touch /tmp/queries/oracle.sql
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/queries:/host:ro ghcr.io/danix1234/oracle-client sh -c 'ls /host/oracle.sql | entr -c sh -c "echo \"\$(cat /host/oracle.sql)\" | sqlplus -s '"$DB_URL"'"'
