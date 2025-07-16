ecd52c2aa35176639278f2c6382b10666a81b781
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-16 11:34:03 +0200

    allow passing db url in mongo run script

--- basi_dati_avanzate/mongodb/run.sh
+++ basi_dati_avanzate/mongodb/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DB_URL="$1"
 mkdir -p /tmp/queries
 touch /tmp/queries/mongo.js
-podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/queries:/host:ro --name mongo-client ghcr.io/danix1234/mongo-client sh -c 'mongod --logpath /dev/null & ls /host/mongo.js | entr -c sh -c "mongosh --quiet --eval \"\$(cat /host/mongo.js)\""'
+podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/queries:/host:ro --name mongo-client ghcr.io/danix1234/mongo-client sh -c 'mongod --logpath /dev/null & ls /host/mongo.js | entr -c sh -c "mongosh '"$DB_URL"' --quiet --eval \"\$(cat /host/mongo.js)\""'
