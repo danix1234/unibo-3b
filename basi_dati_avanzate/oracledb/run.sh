507ccd6aa624497a635abc5a4ed9b048c3e10375
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-07-16 17:55:55 +0200

    fixed script

--- basi_dati_avanzate/oracledb/run.sh
+++ basi_dati_avanzate/oracledb/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DB_URL="$1"
-[[ -z "$DB_URL" ]] && echo 'missing url for oracle connection' && return 1
+[[ -z "$DB_URL" ]] && echo 'missing url for oracle connection' && exit 1
 mkdir -p /tmp/queries
 touch /tmp/queries/oracle.sql
 podman run --rm -it --security-opt label=type:container_runtime_t -v /tmp/queries:/host:ro ghcr.io/danix1234/oracle-client sh -c 'ls /host/oracle.sql | entr -c sh -c "echo \"\$(cat /host/oracle.sql)\" | sqlplus -s '"$DB_URL"'"'
